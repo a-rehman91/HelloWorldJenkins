52c6f0c7459947daaa655e654857c4e595c707d6
a-rehman91 <144312461+a-rehman91@users.noreply.github.com>
2023-09-13 20:31:37 +0500

    .

--- src/com/learn/jenkins/HelloWorld.java
+++ src/com/learn/jenkins/HelloWorld.java
@@ -1,9 +1,26 @@
 package com.learn.jenkins;
 
 public class HelloWorld {
+	
+	class Student{
+		
+		public void Student() {
+			
+			System.out.println("Student Constructor calling...");
+		}
+		
+		public void sayHello() {
+			
+			System.out.println("Hi, I am student...");
+			
+			Student student = new Student();
+			student.sayHello();
+		}
+	}
 
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World Jenkins.");
+		
 	}
 }
